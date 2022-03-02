bd0134eff86642249f1236d8a90f048cf2bdf8dd
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-03-03 01:02:28 +0530

    Time: 10 ms (5.54%), Space: 43.6 MB (5.17%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=1;i<dp.length;i++)
+        {
+            for(int j=1;j<dp[0].length;j++)
+            {
+                if(s.charAt(i-1)==t.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                    
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        if(dp[n][m]==n)
+        {
+            return true;
+        }
+        return false;
+    }
+}
