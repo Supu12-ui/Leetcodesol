ccd7f5d3d1dceea6637ceb89ba18efc842704ac5
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-03-16 08:02:56 +0530

    Added solution - LeetHub

--- Rearrange Array Alternately - GFG/rearrange-array-alternately.java
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -54,30 +54,56 @@ class Solution{
     // n: size of array
     //Function to rearrange  the array elements alternately.
     public static void rearrange(long arr[], int n){
-        
-      long ans[]=new long[n];
-      int start=0;
-      int end=n-1;
-      int i=0;
-      while(start<=end &&i<n)
-      {
-         ans[i]=arr[end];
-          end--;
-          i++;
-          if(i<arr.length)
-          {
-         ans[i]=arr[start];
-          start++;
-          i++;
-          }
-      }
-      for(int k=0;k<n;k++)
-      {
-          arr[k]=ans[k];
-      }
-        
+     long max_idx=n-1; //time complexity
+     long min_idx=0;
+     long max_ele=arr[n-1]+1;
+     for(int i=0;i<n;i++)
+     {
+         if(i%2==0)
+         {
+             arr[i]=arr[i]+(arr[(int)max_idx]%max_ele)*max_ele;
+             max_idx--;
+         }
+         else
+         {
+             arr[i]=arr[i]+(arr[(int)min_idx]%max_ele)*max_ele;
+             min_idx++;
+         }
+        // System.out.println(arr[i]);
+     }
+     for(int i=0;i<n;i++)
+     {
+         arr[i]=arr[i]/max_ele;
+     }
     }
     
 }
+// time complexity - o(n)
+//space complexity - o(n)
+//  public static void rearrange(long arr[], int n){
+        
+//       long ans[]=new long[n];
+//       int start=0;
+//       int end=n-1;
+//       int i=0;
+//       while(start<=end &&i<n)
+//       {
+//          ans[i]=arr[end];
+//           end--;
+//           i++;
+//           if(i<arr.length)
+//           {
+//          ans[i]=arr[start];
+//           start++;
+//           i++;
+//           }
+//       }
+//       for(int k=0;k<n;k++)
+//       {
+//           arr[k]=ans[k];
+//       }
+        
+//     }
+    
 
 
