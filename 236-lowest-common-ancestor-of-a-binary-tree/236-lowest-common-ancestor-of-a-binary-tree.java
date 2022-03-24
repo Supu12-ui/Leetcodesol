8534d7fc0eb00696493746b491002d891f1ea394
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-03-24 23:04:45 +0530

    Time: 8 ms (71.49%), Space: 47.6 MB (28.76%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,46 +9,80 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> ans=new ArrayList<>();
-        ArrayList<ArrayList<TreeNode>> myans=new ArrayList<>();
-        cal(root,p,ans,myans);
-        ans=new ArrayList<>();
-        cal(root,q,ans,myans);
-        int i=0,j=0;
-        while(i<myans.get(0).size() && j<myans.get(1).size())
-        {
-            if(myans.get(0).get(i).val!=myans.get(1).get(j).val)
-            {
-                break;
-            }
-            i++;
-            j++;
-        }
-        return myans.get(0).get(i-1);
-    }
-    boolean cal(TreeNode root,TreeNode p,ArrayList<TreeNode> ans,ArrayList<ArrayList<TreeNode>> myans)
-    {
+         
         if(root==null)
         {
-           return false; 
+            return null;
         }
-        ans.add(root);
-        if(root.val==p.val)
+        if(root==p||root==q)
         {
-            myans.add(new ArrayList<>(ans));
-            return true;
+            return root;
         }
-        boolean check1=cal(root.left,p,ans,myans);
-        boolean check2=cal(root.right,p,ans,myans);
-        if(check1||check2)
+        TreeNode l=lowestCommonAncestor(root.left,p,q);
+        TreeNode r=lowestCommonAncestor(root.right,p,q);
+        if(l!=null && r==null)
         {
-            return true;
+            return l;
+        }
+        else if(l==null && r!=null)
+        {
+            return r;
+        }
+        else if(l!=null && r!=null)
+        {
+            return root;
         }
         else
         {
-            ans.remove(ans.size()-1);
-            return false;
+            return l;
         }
         
-    }
+        
+        
+        
+        
+       
+      }
 }
+//      ArrayList<TreeNode> ans=new ArrayList<>();
+//         ArrayList<ArrayList<TreeNode>> myans=new ArrayList<>();
+//         cal(root,p,ans,myans);
+//         ans=new ArrayList<>();
+//         cal(root,q,ans,myans);
+//         int i=0,j=0;
+//         while(i<myans.get(0).size() && j<myans.get(1).size())
+//         {
+//             if(myans.get(0).get(i).val!=myans.get(1).get(j).val)
+//             {
+//                 break;
+//             }
+//             i++;
+//             j++;
+//         }
+//         return myans.get(0).get(i-1);
+//     }
+//     boolean cal(TreeNode root,TreeNode p,ArrayList<TreeNode> ans,ArrayList<ArrayList<TreeNode>> myans)
+//     {
+//         if(root==null)
+//         {
+//            return false; 
+//         }
+//         ans.add(root);
+//         if(root.val==p.val)
+//         {
+//             myans.add(new ArrayList<>(ans));
+//             return true;
+//         }
+//         boolean check1=cal(root.left,p,ans,myans);
+//         boolean check2=cal(root.right,p,ans,myans);
+//         if(check1||check2)
+//         {
+//             return true;
+//         }
+//         else
+//         {
+//             ans.remove(ans.size()-1);
+//             return false;
+//         }
+        
+//     }
