a0620a124c570f068d2f36b3cba4803d6b5c9fe9
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-03-27 00:25:15 +0530

    Time: 0 ms (100.00%), Space: 49.5 MB (69.28%) - LeetHub

---
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+      if(root==null)
+      {
+          return 0;
+      }
+        int c=0;
+        int l=countNodes(root.left);
+        int r=countNodes(root.right);
+        int ans=l+r;
+        return ans+1;
+    }
+}
