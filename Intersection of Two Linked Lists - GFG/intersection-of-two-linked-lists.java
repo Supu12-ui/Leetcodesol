428d8d6a73addbc83cf30b17ea7429dbd383f688
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-06 01:50:16 +0530

    Added solution - LeetHub

---
+++ Intersection of Two Linked Lists - GFG/intersection-of-two-linked-lists.java
@@ -0,0 +1,122 @@
+// { Driver Code Starts
+//
+
+import java.io.*;
+import java.util.*;
+
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+
+class GfG
+{
+    static Scanner sc = new Scanner(System.in);
+    
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        head = tail = new Node(val);
+        
+        size--;
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int n , m;
+            
+            n = sc.nextInt();
+            Node head1 = inputList(n);
+            
+            m = sc.nextInt();
+            Node head2 = inputList(m);
+            
+            Solution obj = new Solution();
+            
+            Node result = obj.findIntersection(head1, head2);
+            
+            printList(result);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* structure of list Node:
+
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+
+*/
+
+class Solution
+{
+    public static Node findIntersection(Node head1, Node head2)
+    {
+        // add your code here
+        // return the head of intersection list
+        Node dummy=new Node(-1);
+        Node temp1=head1;
+        Node temp2=head2;
+        Node temp3=dummy;
+         HashSet<Integer> hs=new HashSet<>();
+         while(temp2!=null)
+         {
+             hs.add(temp2.data);
+             temp2=temp2.next;
+         }
+         while(temp1!=null)
+         {
+             if(hs.contains(temp1.data))
+             {
+                 Node node=new Node(temp1.data);
+                 temp3.next=node;
+                 temp3=temp3.next;
+             }
+             temp1=temp1.next;
+         }
+         return dummy.next;
+    }
+}
