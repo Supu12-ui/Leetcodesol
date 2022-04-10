1b47a45c7e624d7769bdf69ac2729baca9b8e28b
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-10 21:04:31 +0530

    Time: 3 ms (78.45%), Space: 43.2 MB (7.26%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -1,51 +1,38 @@
 class Solution {
     public int calPoints(String[] ops) {
-        Stack<Integer> st=new Stack<>();
+         ArrayList<Integer> ans=new ArrayList<>();
         for(int i=0;i<ops.length;i++)
         {
-            if(check(ops[i]))
+         if(ops[i].equals("D"))
+         {
+            int value=ans.get(ans.size()-1);
+             value=value*2;
+             ans.add(value);
+             
+         }
+            else if(ops[i].equals("C"))
             {
-                
-                int val=Integer.parseInt(ops[i]);
-                st.push(val);
+                int value=ans.remove(ans.size()-1);
+            }
+            else if(ops[i].equals("+"))
+            {
+                int val1=ans.get(ans.size()-1);
+                int val2=ans.get(ans.size()-2);
+                int sum=val1+val2;
+                ans.add(sum);
             }
             else 
             {
-                if(st.size()>0 && ops[i].equals("D"))
-                {
-                    int value=2*st.peek();
-                    st.push(value);
-                }
-            else if(st.size()>0 && ops[i].equals("C"))
-                {
-                    st.pop();
-                }
-                else if(st.size()>0 && ops[i].equals("+"))
-                {
-                    int val1=st.pop();
-                    int val2=st.pop();
-                    st.push(val2);
-                    st.push(val1);
-                    val1=val1+val2;
-                    st.push(val1);
-                }
-                
+                int value=Integer.valueOf(ops[i]);
+                ans.add(value);
             }
         }
         int sum=0;
-       while(!st.isEmpty())
-       {
-           int val=st.pop();
-           sum=sum+val;
-       }
-        return sum;
-    }
-    boolean check(String str)
-    {
-        if(str.equals("D")||str.equals("C")||str.equals("+"))
+        for(int i=0;i<ans.size();i++)
         {
-            return false;
+            sum=sum+ans.get(i);
         }
-        return true;
+        return sum;
+        
     }
 }
