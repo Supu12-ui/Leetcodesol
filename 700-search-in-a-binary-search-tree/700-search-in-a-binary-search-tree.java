70fd6505e32ec3352124a735d76df5fbaaf58bc6
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-14 20:57:42 +0530

    Time: 0 ms (100.00%), Space: 43 MB (81.70%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -15,10 +15,10 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null)
-        {
-            return null;
-        }
+         if(root==null)
+         {
+             return null;
+         }
         if(root.val==val)
         {
             return root;
