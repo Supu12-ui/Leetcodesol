9054776879d2d14c0341864f8ed07937f797ead1
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-19 20:15:22 +0530

    Time: 24 ms (5.40%), Space: 47.9 MB (14.69%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -14,45 +14,76 @@
  * }
  */
 class Solution {
-    ArrayList<TreeNode> arr=new ArrayList<>();
+    TreeNode prev=null;
+    TreeNode cur=null;
+    TreeNode temp=null;
     public void recoverTree(TreeNode root) {
-        check(root);
-        TreeNode prev=arr.get(0);
-        TreeNode cur=arr.get(1);
-         int i=1;
-        while(i<arr.size())
-        {
-           if(arr.get(i-1).val>arr.get(i).val)
-           {
-               prev=arr.get(i-1);
-              
-               break;
-           }
-            i++;
-        }
-        int temp=prev.val;
-        while(i<arr.size())
-        {
-            if(arr.get(i-1).val>arr.get(i).val)
-            {
-                cur=arr.get(i);
-            }
-            i++;
-        }
-        System.out.println(cur.val+" "+temp);
+       check(root);
+        System.out.println(prev.val+" "+cur.val);
+        int temp1=prev.val;
         prev.val=cur.val;
-        cur.val=temp;
-        
+        cur.val=temp1;
     }
     public void check(TreeNode root)
     {
         if(root==null)
         {
-            return;
+            return ;
         }
         check(root.left);
-        arr.add(root);
+        if(temp!=null && temp.val>root.val)
+        {
+           if(prev==null)
+           {
+               prev=temp;
+           }
+            cur=root;
+        }
+        temp=root;
         check(root.right);
     }
     
 }
+// class Solution {
+//     ArrayList<TreeNode> arr=new ArrayList<>();
+//     public void recoverTree(TreeNode root) {
+//         check(root);
+//         TreeNode prev=arr.get(0);
+//         TreeNode cur=arr.get(1);
+//          int i=1;
+//         while(i<arr.size())
+//         {
+//            if(arr.get(i-1).val>arr.get(i).val)
+//            {
+//                prev=arr.get(i-1);
+              
+//                break;
+//            }
+//             i++;
+//         }
+//         int temp=prev.val;
+//         while(i<arr.size())
+//         {
+//             if(arr.get(i-1).val>arr.get(i).val)
+//             {
+//                 cur=arr.get(i);
+//             }
+//             i++;
+//         }
+//         System.out.println(cur.val+" "+temp);
+//         prev.val=cur.val;
+//         cur.val=temp;
+        
+//     }
+//     public void check(TreeNode root)
+//     {
+//         if(root==null)
+//         {
+//             return;
+//         }
+//         check(root.left);
+//         arr.add(root);
+//         check(root.right);
+//     }
+    
+// }
