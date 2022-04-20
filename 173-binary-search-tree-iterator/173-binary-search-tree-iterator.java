0328da65607d9f4380af3e4fcd34aedc1b229469
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-20 17:41:01 +0530

    Time: 50 ms (5.54%), Space: 57.3 MB (5.80%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -14,44 +14,51 @@
  * }
  */
 class BSTIterator {
-  ArrayList<Integer> ans;
-    int p=0;
+
+    Stack<TreeNode> st;
     public BSTIterator(TreeNode root) {
-        ans=new ArrayList<>();
-        inorder(root);
-        
+        st=new Stack<>();
+         insert(root);
        
-    }
-    public void inorder(TreeNode root)
-    {
-        if(root==null)
-        {
-            return;
-        }
-        inorder(root.left);
-        ans.add(root.val);
-        inorder(root.right);
     }
     
     public int next() {
-      if(hasNext()==true)
-      {
-          int val=ans.get(p);
-          p++;
-          return val;
-      }
-        return -1;
+      // System.out.println(st.pop().val);
+        if(st.size()==0)
+        {
+            return -1;
+        }
+        TreeNode temp=st.pop();
+        System.out.println(temp.val);
+        if(temp.right!=null)
+        {
+           
+            insert(temp.right);
+        }
+        
+        return temp.val;
         
     }
     
     public boolean hasNext() {
-        if(p!=ans.size())
-        {
-            return true;
-        }
-        else
+        //System.out.println(st.pop().val);
+        if(st.size()==0)
         {
             return false;
+        }
+        
+        return true;
+        
+    }
+    public void insert(TreeNode root)
+    {
+        while(root!=null)
+        {
+            
+                st.push(root);
+              
+                root=root.left;
+            
         }
     }
 }
