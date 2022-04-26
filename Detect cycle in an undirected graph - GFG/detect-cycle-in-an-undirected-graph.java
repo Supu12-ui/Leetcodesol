e7847e78e362e800c58fc97fc7755ca36115e1f5
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-27 00:29:33 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -34,55 +34,41 @@ class GFG {
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
         boolean vis[]=new boolean[V];
-       for(int i=0;i<V;i++)
-       {
-           if(vis[i]==false)
-           {
-           Queue<pair> pq=new LinkedList<>();
-           vis[i]=true;
-            pair p=new pair(i,-1);
-            pq.add(p);
-          if(check(adj,vis,pq))
-          {
-              return true;
-          }
-           }
-           
-       }
-       return false;
-    }
-    boolean check(ArrayList<ArrayList<Integer>> adj,boolean vis[],Queue<pair> pq)
-    {
-      while(pq.size()>0)
-      {
-         pair p=pq.poll();
-         for(int j=0;j<adj.get(p.cur).size();j++)
-         {
-             int val=adj.get(p.cur).get(j);
-             if(vis[val]==true && val!=p.prev)
-             {
-                 return true;
-             }
-            else if(vis[val]==false) 
-             {
-                 vis[val]=true;
-                 pair p1=new pair(val,p.cur);
-                 pq.add(p1);
-             }
-         }
-        
-      }
-       return false;
-    }
-    class pair
-    {
-        int cur;
-        int prev;
-        public pair(int cur,int prev)
+        boolean ans=false;
+        for(int i=0;i<V;i++)
         {
-          this.cur=cur;
-          this.prev=prev;
+            if(vis[i]==false)
+            {
+              if(dfs(vis,adj,i,-1))
+              {
+                  return true;
+              }
+            }
         }
+        return ans;
+        
+    }
+    boolean dfs(boolean vis[],ArrayList<ArrayList<Integer>> adj,int cur,int prev)
+    {
+        vis[cur]=true;
+        for(int i=0;i<adj.get(cur).size();i++)
+        {
+            int val=adj.get(cur).get(i);
+            if(vis[val]==true && val!=prev)
+            {
+                return true;
+            }
+            else if(vis[val]==false)
+            {
+                boolean haspath=dfs(vis,adj,val,cur);
+                if(haspath==true)
+            {
+                return true;
+            }
+            }
+        }
+        return false;
     }
 }
