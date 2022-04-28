a84076f8f44139aaa09f1af710dbf669fb0bcc90
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-04-28 20:36:49 +0530

    Added solution - LeetHub

---
+++ Bipartite Graph - GFG/bipartite-graph.java
@@ -0,0 +1,89 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(S[0]);
+            int E = Integer.parseInt(S[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for(int i = 0; i < V; i++){
+                adj.add(new ArrayList<Integer>());
+            }
+            for(int i = 0; i < E; i++){
+                String[] s = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(s[0]);
+                int v = Integer.parseInt(s[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isBipartite(V, adj);
+            if(ans)
+                System.out.println("1");
+            else System.out.println("0");
+       }
+    }
+}// } Driver Code Ends
+
+
+class Solution
+{
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int vis[]=new int[V];
+        for(int i=0;i<vis.length;i++)
+        {
+            vis[i]=-1;
+        }
+        Queue<Integer> pq=new LinkedList<>();
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i]==-1)
+            {
+                vis[i]=1;
+                pq.add(i);
+               if(! dfs(adj,vis,pq)){
+                   return false;
+                  }
+               
+            }
+        }
+        return true;
+        
+    }
+    boolean dfs(ArrayList<ArrayList<Integer>> adj,int vis[],Queue<Integer> q)
+    {
+        while(q.size()!=0)
+        {
+            int val=q.peek();
+            q.poll();
+            for(int i=0;i<adj.get(val).size();i++)
+            {
+                int check=adj.get(val).get(i);
+                if(vis[check]==-1)
+                {
+                    q.add(check);
+                   vis[check]=1-vis[val];
+                }
+                else if(vis[val]==vis[check])
+                {
+                    
+                    
+                        return false;
+                    
+                }
+                
+            }
+            
+        }
+        return true;
+    }
+}
