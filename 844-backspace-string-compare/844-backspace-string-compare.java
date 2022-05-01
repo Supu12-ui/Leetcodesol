44ab484d3566fa3ca23d4321fcb51dcdfd954197
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-05-02 01:02:51 +0530

    Time: 9 ms (7.93%), Space: 42.1 MB (52.91%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -2,6 +2,38 @@ class Solution {
     public boolean backspaceCompare(String s, String t) {
       String ans1=solve(s);
         String ans2=solve(t);
+        System.out.println(ans1+" "+ans2);
+        
+        if(ans1.equals(ans2))
+        {
+            return true;
+        }
+        return false;
+      
+    }
+    String solve(String s)
+    {
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='#'&& ans.length()!=0)
+            {
+                ans.deleteCharAt(ans.length()-1);
+            }
+            else if(s.charAt(i)!='#')
+            {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
+
+
+class solution {
+    public boolean backspaceCompare(String s, String t) {
+      String ans1=solve(s);
+        String ans2=solve(t);
         if(ans1.equals(ans2))
         {
             return true;
