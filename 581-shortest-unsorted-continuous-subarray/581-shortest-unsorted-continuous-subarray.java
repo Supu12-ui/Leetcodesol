c637d22e1004e63b5a19f7bc33a17b79b42eea4a
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-05-03 20:11:29 +0530

    Time: 72 ms (5.13%), Space: 54.7 MB (10.71%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -1,5 +1,62 @@
 class Solution {
     public int findUnsortedSubarray(int[] nums) {
+       Stack<Integer> st=new Stack<>();
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(st.size()==0)
+            {
+                st.push(i);
+            }
+            else if(nums[st.peek()]<=nums[i])
+            {
+                st.push(i);
+            }
+            else
+            {
+                while(st.size()!=0 && nums[st.peek()]>nums[i])
+                {
+                   
+              
+                    min=Math.min(min,st.peek());
+                     st.pop();
+                }
+                st.push(i);
+            }
+        
+        }
+        st.clear();
+        for(int i=n-1;i>=0;i--)
+        {
+           if(st.size()==0)
+           {
+               st.push(i);
+           }
+            else if(nums[st.peek()]>=nums[i])
+            {
+                st.push(i);
+            }
+            else
+            {
+               while(st.size()!=0 && nums[st.peek()]<nums[i])
+                {
+                  max=Math.max(max,st.peek());
+                   st.pop();
+                }
+                st.push(i); 
+            }
+        }
+        if(max==Integer.MIN_VALUE && min==Integer.MAX_VALUE)
+        {
+            return 0;
+        }
+        return (max-min)+1;
+    }
+}
+class solution {
+    public int findUnsortedSubarray(int[] nums) {
         int n=nums.length;
         int a[]=new int[nums.length];
         for(int i=0;i<n;i++)
