ae2687bb7fa6d24c2fedaca4434e8dd8dc92cf6e
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-05-19 07:31:45 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (77.91%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,24 +15,20 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> ar=new ArrayList<>();
-        Stack<TreeNode> st=new Stack<>();
-        TreeNode node=root;
-       
-        while(node !=null ||st.size()>0)
-        {
-            while(node!=null)
-            {
-                st.push(node);
-               
-                node=node.left;
-            }
-           
-            node = st.peek();
-            st.pop();
-            ar.add(node.val);
-            node=node.right;
-        }
-        return ar;
+        List<Integer> ans=new ArrayList<>();
+        solve(root,ans);
+        return ans;
+        
+    }
+    void solve(TreeNode root,List<Integer> ans)
+    {
+      if(root==null)
+      {
+          return;
+      }
+      solve(root.left,ans);
+      ans.add(root.val);
+      solve(root.right,ans);
+        
     }
 }
