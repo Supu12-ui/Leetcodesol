5fe1acb5ce933ce07a4153d5afc546b0ae30f917
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-07-06 01:40:01 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (57.81%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -13,69 +13,25 @@
  *     }
  * }
  */
-//postorder means left ,right,root
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> ans=new ArrayList<>();
-           Stack<TreeNode> st=new Stack<>();
-            TreeNode cur=root;
-            while(cur!=null ||st.size()>0)
-            {
-                    if(cur!=null)
-                    {
-                            st.push(cur);
-                            cur=cur.left;
-                    }
-                    else
-                    {
-                           cur=st.peek().right;
-                            if(cur==null)
-                            {
-                              TreeNode temp=st.pop();
-                                ans.add(temp.val);
-                                 while(st.size()>0 && temp==st.peek().right)
-                                 {
-                                     temp=st.pop();
-                                     ans.add(temp.val);
-                                     
-                                     
-                                 }
-                            }
-                        
-                            
-                    }
-            }
+        List<Integer> ans=new ArrayList<>();
+        sol(root,ans);
         return ans;
+        
+    }
+    public void sol(TreeNode root,List<Integer> ans)
+    {
+        if(root==null)
+        {
+            return ;
+        }
+        sol(root.left,ans);
+        sol(root.right,ans);
+        ans.add(root.val);
+        
+        
+        
+        
     }
 }
-//  if(root==null)
-//             {
-//                     return Collections.emptyList();
-//             }
-        
-//         Stack<TreeNode> st1=new Stack<>();
-//         Stack<TreeNode> st2=new Stack<>();
-//             st1.push(root);
-//         TreeNode cur=root;
-//         while(st1.size()>0)
-//         {
-//             TreeNode temp=st1.pop();
-//             st2.push(temp);
-//             if(temp.left!=null)
-//             {
-//                 st1.push(temp.left);
-                
-//             }
-//             if(temp.right!=null)
-//             {
-//                 st1.push(temp.right);
-//             }
-//         }
-//         ArrayList<Integer> ar=new ArrayList<>();
-//         while(st2.size()>0)
-//         {
-//             TreeNode temp=st2.pop();
-//             ar.add(temp.val);
-            
-//         }
-//         return ar;
