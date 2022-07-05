855169da4193c34c9afa9785b9279c0679de522b
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-07-05 18:00:01 +0530

    Time: 1 ms (48.27%), Space: 42.1 MB (64.92%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -13,78 +13,20 @@
  *     }
  * }
  */
-//morris traveral
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
-        TreeNode cur=root;
-        while(cur!=null)
-        {
-            if(cur.left==null)
-            {
-                ans.add(cur.val);
-                cur=cur.right;
-            }
-            else
-            {
-                TreeNode imp=cur.left;
-                while(imp.right!=null && imp.right!=cur)
-                {
-                imp=imp.right;
-                }
-                if(imp.right==null)
-                {
-                    imp.right=cur;
-                    cur=cur.left;
-                }
-                else
-                {
-                    ans.add(cur.val);
-                    imp.right=null;
-                    cur=cur.right;
-                }
-            }
-        }
+        sol(root,ans);
         return ans;
     }
-}
-class solution1 {
-    public List<Integer> inorderTraversal(TreeNode root) {
-     List<Integer> ans=new ArrayList<>();
-     Stack<TreeNode> st=new Stack<>();
-     TreeNode cur=root;
-     while(st.size()!=0 || cur!=null)
-     {
-         while(cur!=null)
-         {
-             st.push(cur);
-             cur=cur.left;
-         }
-         cur=st.pop();
-         ans.add(cur.val);
-         cur=cur.right;
-     }
-        return ans;
-    }
-}
-
-
-class solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
-        solve(root,ans);
-        return ans;
-        
-    }
-    void solve(TreeNode root,List<Integer> ans)
+    public void sol(TreeNode root,List<Integer> ans)
     {
-      if(root==null)
-      {
-          return;
-      }
-      solve(root.left,ans);
-      ans.add(root.val);
-      solve(root.right,ans);
-        
+       if(root==null)
+       {
+           return ;
+       }
+       sol(root.left,ans);
+        ans.add(root.val);
+        sol(root.right,ans);
     }
 }
