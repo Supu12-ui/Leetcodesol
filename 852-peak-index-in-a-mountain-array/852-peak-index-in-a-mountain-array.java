ff6ba4cab43f5e3ea92357084eb01dfe7b2df105
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-07-09 02:03:26 +0530

    Time: 0 ms (100.00%), Space: 73 MB (7.51%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+      int start=0; 
+      int end=arr.length-1;
+        int ans=0;
+      while(start<end)
+      {
+          int mid=(start+end)/2;
+          if(arr[mid]>arr[mid+1])
+          {
+             ans=mid;
+              end=mid;
+          }
+          else
+          {
+              start=mid+1;
+          }
+      }
+        return ans;
+    }
+}
