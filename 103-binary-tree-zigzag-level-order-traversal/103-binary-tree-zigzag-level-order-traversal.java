1b8895bb8323426c233d2dcbf6445725d7f382bf
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-07-18 02:03:12 +0530

    Time: 1 ms (96.25%), Space: 42.9 MB (35.01%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -15,77 +15,49 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        
+    
+    
         List<List<Integer>> ans=new ArrayList<>();
-        cal(ans,0,root);
-        
+        if(root==null)
+        {
+            return ans;
+        }
+        Queue<TreeNode> pq=new LinkedList<>();
+        pq.add(root);
+        int count=1;
+        while(pq.size()>0)
+        {
+          int size=pq.size();
+          List<Integer> temp=new ArrayList<>();
+          for(int i=0;i<size;i++)
+          {
+              TreeNode t=pq.remove();
+              
+              if(count%2==0)
+              {
+                temp.add(0,t.val); 
+              }
+              else
+              {
+               temp.add(t.val);   
+              }
+              if(t.left!=null)
+              {
+                  pq.add(t.left);
+              }
+              
+              if(t.right!=null)
+              {
+                  pq.add(t.right);
+              }
+             
+          }
+            count++;
+            ans.add(new ArrayList<>(temp));
+            
+        }
         return ans;
-        
-    }
-    void cal(List<List<Integer>> ar,int level,TreeNode root)
-    {
-       if(root==null)
-       {
-           return;
-       }
-        if(ar.size()<=level)
-        {
-            ar.add(new ArrayList<>());
-        }
-        if(level%2==0)
-        {
-            ar.get(level).add(root.val);
-        }
-        else
-        {
-            ar.get(level).add(0,root.val);
-        }
-        cal(ar,level+1,root.left);
-        cal(ar,level+1,root.right);
     }
     
     
-    
-//     TreeNode cur=root;
-//         List<List<Integer>> ans=new ArrayList<>();
-//         if(root==null)
-//         {
-//             return ans;
-//         }
-//         Queue<TreeNode> pq=new LinkedList<>();
-//          boolean flag=false;
-//         pq.add(root);
-//         while(pq.size()>0)
-//         {
-           
-//             int size=pq.size();
-//            List<Integer> ar=new ArrayList<>();
-//             for(int i=0;i<size;i++)
-//             {
-//                 TreeNode temp=pq.peek();
-//                 pq.remove();
-//                 if(temp.left!=null)
-//                 {
-//                     pq.add(temp.left);
-//                 }
-//                 if(temp.right!=null)
-//                 {
-//                     pq.add(temp.right);
-//                 }
-//                 ar.add(temp.val);
-//             }
-//             if(flag==true)
-//             {
-//                 Collections.reverse(ar);
-//                 System.out.println(ar);
-//                 ans.add(ar);
-//             }
-//             else
-//             {
-//                 ans.add(ar);
-//             }
-//             flag=!flag;
-//         }
-//         return ans;
-        
 }
