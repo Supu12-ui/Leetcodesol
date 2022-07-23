f4c60cb9f44147df166169aedfd980be9e3c5b9b
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2022-07-23 16:49:40 +0530

    Time: 0 ms (100.00%), Space: 43 MB (88.55%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -5,17 +5,17 @@ class Solution {
         while(start<=end)
         {
             int mid=(start+end)/2;
-            if(nums[mid]==target)
+            if(target==nums[mid])
             {
                 return mid;
             }
-            else if(nums[mid]>target)
+            else if(nums[mid]<target)
             {
-                end=mid-1;
+                start=mid+1;
             }
             else
             {
-               start=mid+1;
+                end=mid-1;
             }
         }
         return -1;
