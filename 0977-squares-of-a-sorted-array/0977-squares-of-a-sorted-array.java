1cd5f0a45463fc7ca870bc8c486fd47b1dbe0777
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2023-02-15 15:24:14 +0530

    Time: 28 ms (10.82%), Space: 44.3 MB (33.27%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int end=nums.length-1;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(int i=0;i<nums.length;i++)
         {
             nums[i]=nums[i]*nums[i];
-            //System.out.println(nums[i]);
+            pq.add(nums[i]);
+           
         }
-        for(int i=0;i<nums.length;i++)
+        int i=0;
+        while(pq.size()>0)
         {
-            int start=i;
-            end=nums.length-1;
-            while(start<end)
-            {
-              if(nums[start]>nums[end])
-              {
-                  int temp=nums[start];
-                  nums[start]=nums[end];
-                  nums[end]=temp;
-                  end--;
-              }
-              else
-              {
-                  end--;
-              }
-            }
+            int val=pq.poll();
+            nums[i]=val;
+            i++;
         }
+        
         return nums;
     }
     
