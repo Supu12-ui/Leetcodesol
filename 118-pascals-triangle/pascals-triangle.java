75efb012b1a20ab2db2cc5fad978435315500a2e
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2024-07-02 22:50:40 +0530

    Time: 7 ms (5.16%) | Memory: 42.3 MB (6.22%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+      List<List<Integer>> ans =new ArrayList<>();
+     
+      for(int i=0;i<numRows;i++)
+      {
+          List<Integer> res = new ArrayList<>();
+          if(i==0)
+          {
+              res.add(1);
+          }
+          else if (i==1)
+          {
+             res.add(1);
+              res.add(1);
+          }
+          else{
+              int size = ans.get(i-1).size();
+              res.add(1);
+              for(int j=1;j<size;j++)
+              {
+                  int k = ans.get(i-1).get(j-1)+ans.get(i-1).get(j);
+                  res.add(k);
+              }
+              res.add(1);
+              System.out.println(res);
+          }
+          ans.add(res);
+      }
+        return ans;
+    }
+}
