6ac27d32c4cfbc52ff322191299a31014fb7e14b
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2024-07-02 13:10:55 +0530

    Update set-matrix-zeroes.java

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,3 +1,84 @@
+// brute force approach 
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int n=matrix.length;
+        int m= matrix[0].length;
+        boolean row = false;
+        boolean col=false;
+        for (int i=0;i<n;i++)
+        {
+         for(int j=0;j<m;j++)
+         {
+             if (matrix[i][j]==0){
+              for(int k=0;k<m;k++)
+              {
+                  if(matrix[i][k]==0) continue;
+                      
+                  matrix[i][k]=Integer.MAX_VALUE;
+              }
+                 for(int k=0;k<n;k++)
+                 {
+                     if(matrix[k][j]==0) continue;
+                     //System.out.println(k);
+                     matrix[k][j]=Integer.MAX_VALUE;
+                 }
+             }
+         }
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                //System.out.print(matrix[i][j]);
+                if(matrix[i][j]==Integer.MAX_VALUE)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+            //System.out.println();
+        }
+        
+        
+    }
+}
+// better approach 
+class Solution {
+    public void setZeroes(int[][] matrix) {
+      int n= matrix.length;
+      int m= matrix[0].length;
+      int []row= new int[n];
+      int []col= new int[m];
+      for(int i=0;i<n;i++)
+      {
+          for(int j=0;j<m;j++)
+          {
+              if(matrix[i][j]== 0)
+              {
+                  row[i]=1;
+                  col[j]=1;
+              }
+
+          }
+      }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(row[i]==1 || col[j]==1)
+                {
+                    matrix[i][j]=0;
+                }
+                else
+                {
+                   continue;
+                }
+            }
+        }
+    
+    }
+}
+
+//optimize approach 
 class Solution {
     public void setZeroes(int[][] matrix) {
     int n= matrix.length;
